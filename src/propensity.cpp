5be9c1a5e5fafa4af6d18fcd8c306d050b86ee90
ahemaesh <ahemaesh@andrew.cmu.edu>
2019-10-31 07:24:08 -0400

    propensity initial structure

---
+++ src/propensity.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+#include "propensity.h"
+
+namespace  DynamicSLAM{
+	namespace Propensity{
+		void Propensity::calculatePropensity(std::vector<float> &pointValues){
+			std::cout<< i << std::endl;
+			
+		}
+	}
+}
+
+
+// int main(){
+// 	DynamicSLAM::Propensity::Propensity object_propensity = DynamicSLAM::Propensity::Propensity();
+// 	std::vector<float> nums;
+// 	object_propensity.calculatePropensity(nums);
+// }
---
+++ src/propensity.h
@@ -0,0 +1,27 @@
+#include <vector>
+
+namespace DynamicSLAM{
+	namespace Propensity{
+		class Propensity{
+			private:
+				//place holder
+				static const int i = 144;
+			
+			public:
+				Propensity(){
+					// Initialize variables
+				}
+				
+				~Propensity(){
+					// Destroy variables/clear memory
+				}
+
+
+				void calculatePropensity(std::vector<float> &pointValues);
+
+		};
+	}
+}
+
+
+
